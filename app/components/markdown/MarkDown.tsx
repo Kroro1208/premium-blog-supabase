b19d2902a35afb88c3e4f0850601468a05be8c6e
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-08-16 17:04:12 +0900

    タイトル、画像、コンテント（コード入力まで）ができるようにした

--- app/components/markdown/MarkDown.tsx
+++ app/components/markdown/MarkDown.tsx
@@ -2,6 +2,9 @@ import { cn } from "@/lib/utils";
 import Markdown from "react-markdown"
 import rehypeHighlight from "rehype-highlight"
 import "highlight.js/styles/atom-one-dark.min.css"
+import { PiTerminalThin } from "react-icons/pi";
+import CopyButton from "./CopyButton";
+import { icons } from "@/lib/icons";
 
 const MarkDown = ({ content, className }: { content: string; className?: string; }) => {
     return (
@@ -21,9 +24,35 @@ const MarkDown = ({ content, className }: { content: string; className?: string;
                 code: ({ node, className, children, ...props }) => {
                     const match = /language-(\w+)/.exec(className || "");
                     if (match?.length) {
+                        let Icon = PiTerminalThin
+                        const isMatch = icons.hasOwnProperty(match[1]);
+                        if (isMatch) {
+                            Icon = icons[match[1] as keyof typeof icons];
+                        }
+                        const id = (
+                            Math.floor(Math.random() * 100 + 1).toString()
+                        );
 
+                        return (
+                            <div className="bg-gradient-dark text-gray-300 border rounded-md">
+                                <div className="px-5 py-2 border-b flex items-center justify-between">
+                                    <div className="flex items-center gap-2">
+                                        <Icon />
+                                        <span>
+                                            {
+                                                node?.data?.meta
+                                            }
+                                        </span>
+                                    </div>
+                                    <CopyButton id={id} />
+                                </div>
+                                <div className="overflow-x-auto w-full">
+                                    <div className="p-5" id={id}>{children}</div>
+                                </div>
+                            </div>
+                        );
                     } else {
-                        return <code className="bg-red-400">{children}</code>
+                        return <code className="bg-zinc-700 px-2 rounded-md">{children}</code>
                     }
                 }
             }}>
--- app/dashboard/blog/create/page.tsx
+++ app/dashboard/blog/create/page.tsx
@@ -32,7 +32,21 @@ const FormSchema = z.object({
     }),
     is_free: z.boolean(),
     is_premium: z.boolean(),
-})
+}).refine((data) => {
+    const image_url = data.image_url
+    try {
+        const url = new URL(image_url);
+        return url.hostname === "images.unsplash.com";
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+},
+    {
+        message: 'サポートされていない画像がアップロードされています',
+        path: ["image_url"]
+    }
+);
 
 export default function BlogForm() {
     const [isPreview, setPreview] = useState(false);
@@ -61,12 +75,12 @@ export default function BlogForm() {
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="w-full rounded-md border space-y-6">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-full rounded-md border space-y-6 pb-10">
                 <div className="p-5 gap-5 flex flex-wrap items-center justify-between">
                     <div className="flex gap-5 items-center border-b flex-wrap">
                         <span role="button" tabIndex={0}
                             className="flex items-center gap-1 border bg-zinc-700 p-2 rounded-lg hover:ring-2 hover:ring-green-500"
-                            onClick={() => setPreview(!isPreview)}
+                            onClick={() => setPreview(!isPreview && !form.getFieldState("image_url").invalid)}
                         >{isPreview ? (
                             <>
                                 <Pencil1Icon />
@@ -151,11 +165,8 @@ export default function BlogForm() {
                                             isPreview ? "w-0 p-0" : "w-full lg:w-1/2")}
                                     />
                                     <div className={cn("lg:px-10", isPreview ? "w-full mx-auto lg:w-4/5" : "w-1/2 lg:blok")}>
-                                        {isPreview ? (
-                                            <>
-                                                画像プレビューを表示
-                                            </>)
-                                            : (
+                                        {isPreview ?
+                                            (
                                                 <>
                                                     <div className="relative h-80 mt-5 border rounded-md">
                                                         <Image src={form.getValues().image_url} alt="preview" fill
@@ -163,6 +174,9 @@ export default function BlogForm() {
                                                         />
                                                     </div>
                                                 </>
+                                            )
+                                            : (
+                                                <>画像プレビューを表示</>
                                             )}
                                     </div>
                                 </div>
@@ -186,7 +200,7 @@ export default function BlogForm() {
                                         className={cn("border-none text-lg font-medium leading-relaxed resize-none",
                                             isPreview ? "w-0 p-0" : "w-full lg:w-1/2")}
                                     />
-                                    <div className={cn("lg:px-10", isPreview ? "w-full mx-auto lg:w-4/5" : "w-1/2 lg:blok")}>
+                                    <div className={cn("overflow-y-auto", isPreview ? "w-full mx-auto lg:w-4/5" : "w-1/2 lg:blok")}>
                                         <MarkDown content={form.getValues().content} />
                                     </div>
                                 </div>
